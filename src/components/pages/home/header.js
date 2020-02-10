ba68e799b16d1d97f4f23553136afdf510d41cd3
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-02-10 11:05:04 -0800

    Added: boiler plate for header

---
+++ src/components/pages/home/header.js
@@ -0,0 +1,15 @@
+// Native Imports
+import React, { Component } from 'react'
+
+// CSS
+import styles from './home.module.css'
+
+export default class header extends Component {
+    render() {
+        return (
+            <div className={styles.test}>
+                Hello
+            </div>
+        )
+    }
+}
