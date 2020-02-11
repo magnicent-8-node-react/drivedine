5cdd24c10ac5c1b1d680bae7372eea6c4ec9720c
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-02-10 16:43:36 -0800

    Added: Landing component

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react'
 
 // Components
 import Header from './header'
+import Landing from './landing'
 
 export default class home extends Component {
     render() {
         return (
             <div>
                 <Header />
+                <Landing />
             </div>
         )
     }
