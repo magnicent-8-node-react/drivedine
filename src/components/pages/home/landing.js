b23c933c5c935a6c5a63d7bd33e389ceb19710d0
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-02-11 09:30:31 -0800

    Implemented new styles

--- src/components/pages/home/landing.js
+++ src/components/pages/home/landing.js
@@ -8,15 +8,33 @@ import phone from '../../../display.png'
 import styles from './home.module.css'
 
 export default class landing extends Component {
+    state = {
+        login: true
+    }
+
     render() {
         return (
-            <div className={`d-flex flex-direction-row flex-wrap`}>
-                <div className={`col-md-6`}>
-                    Hello
+            <div className={`d-flex flex-direction-row flex-wrap ${styles.landing_section}`}>
+                <div className={`col-md-6 ${styles.text_section} ${styles.test_b}`}>
+                    <div className={`${styles.text_wrap} ${styles.test_b}`}>
+                        <h1>DriveDine for finding trucks</h1>
+                        <p>
+                            Cillum eiusmod voluptate commodo consectetur nisi reprehenderit
+                            in veniam mollit eiusmod nulla adipisicing incididunt.
+                            Cillum eiusmod voluptate commodo consectetur nisi reprehenderit
+                            in veniam mollit eiusmod nulla adipisicing incididunt.
+                        </p>
+                        <div className={`${styles.land_button_wrap}`}>
+                            <button className={`${styles.left_land_button}`}>View Local Map</button>
+                            {!this.state.login ? <button>Login</button> : <button>Sign Up</button>}
+                        </div>
+                        
+
+                    </div>
                 </div>
 
-                <div className={`d-flex justify-content-center col-md-6`}>
-                    <img className={styles.landing_img} src={phone}/>
+                <div className={`d-flex justify-content-center col-md-6 ${styles.phone_section} ${styles.test_b}`}>
+                    <img alt="phone" className={styles.landing_img} src={phone}/>
                 </div>
             </div>
         )
