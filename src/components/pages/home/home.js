e893e6909086bf3aa884294237dbf22e313b3ace
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-02-11 12:21:13 -0800

    Removed header

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -2,14 +2,12 @@
 import React, { Component } from 'react'
 
 // Components
-import Header from './header'
 import Landing from './landing'
 
 export default class home extends Component {
     render() {
         return (
             <div>
-                <Header />
                 <Landing />
             </div>
         )
