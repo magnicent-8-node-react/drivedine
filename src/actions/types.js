4699f47b65e93b54a465b2323d6ec84a652f181a
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-02-12 10:57:14 -0800

    Added: types for form

--- src/actions/types.js
+++ src/actions/types.js
@@ -7,3 +7,7 @@ export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
+
+// FORM
+export const FORM_TOGGLE = 'FORM_TOGGLE';
+export const FORM_TYPE_TOGGLE = 'FORM_TYPE_TOGGLE';
