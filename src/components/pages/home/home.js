ff5acc0ee234e9622aba13bb82a08c4286d297ce
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-02-12 10:59:18 -0800

    Added: form

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -1,13 +1,15 @@
 // Native Imports
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 // Components
-import Landing from './landing'
+import Landing from './landing';
+import Form from '../form/form.js'
 
 export default class home extends Component {
     render() {
         return (
             <div>
+                <Form />
                 <Landing />
             </div>
         )
