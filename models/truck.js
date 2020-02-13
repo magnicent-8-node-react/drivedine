ddf05b8a5989637769a2601c4a9b6dc93d69137e
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-02-13 15:03:41 -0800

    Added: truck model geocoder

--- models/truck.js
+++ models/truck.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const geocoder = require('../utils/geocoder')
 //
 const TruckSchema = new mongoose.Schema({
     // truckId: {
@@ -28,6 +28,21 @@ const TruckSchema = new mongoose.Schema({
       type: Date,
       default: Date.now
     }
+});
+
+// Geocoder
+TruckSchema.pre('save', async function(next){
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress
+    }
+    console.log(loc);
+
+    // Do not save address
+    this.address = undefined;
+    next();
 });
 
 module.exports = mongoose.model('Truck', TruckSchema); 
