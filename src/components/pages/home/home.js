bdeb2504f793cd86d5dd57c21a5d5095ea8e34ab
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-02-12 16:03:49 -0800

    Added: current geolocation

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -6,6 +6,12 @@ import Landing from './landing';
 import Form from '../form/form.js'
 
 export default class home extends Component {
+    componentDidMount(){
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+        });
+    }
+
     render() {
         return (
             <div>
