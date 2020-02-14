343dd72c85b15f263cff6db524f6338c71e8f629
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-02-13 20:39:54 -0800

    Removed form component

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -3,13 +3,11 @@ import React, { Component } from 'react';
 
 // Components
 import Landing from './landing';
-import Form from '../form/form.js'
 
 export default class home extends Component {
     render() {
         return (
             <div>
-                <Form />
                 <Landing />
             </div>
         )
