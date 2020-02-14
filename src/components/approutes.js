338b036554eaef221001f22b82140ca5a5fa7702
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-02-13 19:18:26 -0800

    Added: location gathering

--- src/components/approutes.js
+++ src/components/approutes.js
@@ -5,19 +5,25 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 // Redux
 import { connect } from 'react-redux'
 
+// Actions
+import { getLocation } from '../actions/auth'
+
 // Components
 import Home from './pages/home/home.js'
 import NavBar from './pages/navbar'
 import MapPage from './pages/map/map.js'
 
 class AppRoutes extends Component {
+  componentDidMount() {
+    this.props.getLocation();
+  }
 
   render() {
     return (
       <Router>
         <Route path="/" component={NavBar}/>
         <Route exact path="/" component={Home}/>
-        <Route path="/products" component={MapPage}/>
+        <Route exact path="/map" component={MapPage}/>
       </Router>
     )
   }
@@ -27,4 +33,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 })
 
-export default connect(mapStateToProps)(AppRoutes)
+export default connect(mapStateToProps, { getLocation })(AppRoutes)
