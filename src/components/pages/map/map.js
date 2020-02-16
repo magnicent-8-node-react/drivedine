e7810f240813d96a40c9385ae7a363c6b5ee52fb
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-02-16 00:03:12 -0800

    Added: map functionality

--- src/components/pages/map/map.js
+++ src/components/pages/map/map.js
@@ -1,111 +1,63 @@
 // Native Imports
 import React, { Component } from 'react'
 
-// Mapbox
-import mapboxgl from 'mapbox-gl'
-
 // Redux
 import { connect } from 'react-redux'
 
 // Actions
-import { getLocation } from '../../../actions/auth'
+import { getTrucks, getLocation, jumpTo } from '../../../actions/location'
 
 // CSS
 import styles from './map.module.css'
 
-// Test Image
-import icon from '../../../pointer.png'
-import icon2 from '../../../logo.png'
+// Mapbox
+import mapboxgl from 'mapbox-gl'
 
 // Set Mapbox Token
 mapboxgl.accessToken = 'pk.eyJ1IjoiZHJpdmVkaW5lMiIsImEiOiJjazZsYzRwdmUwZG1lM211OXNlb2JsYnRiIn0.ZDOFdh3cuDMQZt9FPnZ-jw';
 
 // Mapbox class
 class Map extends Component {
-    componentDidMount() {
-        this.props.getLocation();
-        let { lat, long } = this.props.auth.location
-        
-        const map = new mapboxgl.Map({
-            container: this.mapContainer,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [long, lat],
-            zoom: 14
-        });
+  componentDidMount () {
+    this.props.getLocation();
+    this.renderMap();
+  }
 
-        async function getTrucks() {
-            const res = await fetch('https://drivedineapi.herokuapp.com/api');
-            const data = await res.json();          
-            const trucks = data.data.map(truck => {
-              return {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [
-                    truck.location.coordinates[0],
-                    truck.location.coordinates[1]
-                  ]
-                },
-                properties: {
-                  truckName: truck.truckName,
-                  icon: 'icon'
-                }
-              };
-            });
-            loadMap(trucks);
-        }
+  renderMap = () => {
+    let { long, lat } = this.props.auth.location;
 
-        function loadMap(trucks) {
-            map.on('load', function() {
-                // Load Images
-                map.loadImage( icon, (error, image) => {
-                    if (error) throw error;
-                    map.addImage('icon', image);}
-                );
-                map.loadImage( icon2, (error, image) => {
-                    if (error) throw error;
-                    map.addImage('icon2', image);}
-                );
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [long, lat],
+      zoom: 14
+    })
+    
+    // Sets map to Component State for Reference
+    this.setState({map})
+    getTrucks(map);
 
-                map.addLayer({
-                    id: 'points',
-                    type: 'symbol',
-                    source: {
-                      type: 'geojson',
-                      data: {
-                        type: 'FeatureCollection',
-                        features: trucks
-                      }
-                    },
-                    layout: {
-                      'icon-image': '{icon}',
-                      'icon-size': .8,
-                      'text-field': '{truckName}',
-                      'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-                      'text-offset': [0, 0.9],
-                      'text-anchor': 'top'
-                    }
-                });
-            });
-        }
-        getTrucks();
-    }
+    // Removes All Map Options
+    this.mapContainer.removeChild(this.mapContainer.childNodes[2])
+  }
 
-    render() {
-        return (
-            <div className={`d-flex justify-content-center ${styles.map_wrap}`}>
-                <div ref={el => this.mapContainer = el} className={`mapContainer ${styles.map_container}`}/>
-            </div>
-        )
-    }
+  reloadLocation = () => {
+    let { lat, long } = this.props.auth.location;
+    jumpTo(this.state.map, long, lat);
+  }
+
+  render() {
+    return (
+        <div className={`d-flex justify-content-center ${styles.map_wrap}`}>
+            <div ref={el => this.mapContainer = el } className={`mapContainer ${styles.map_container}`}/>
+            <button className={`${styles.reload_btn}`} onClick={this.reloadLocation}>Refresh Location</button>
+        </div>
+    )
+  }
 }
 
-// const mapStateToProps = (state) => ({
-//     auth: state.auth
-// })
-
 const mapStateToProps = (state) => {
-    return { auth: state.auth }
+  return { auth: state.auth }
 }
 
 export default connect(mapStateToProps, { getLocation })(Map)
