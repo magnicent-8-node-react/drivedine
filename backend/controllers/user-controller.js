4f8f1c08a91fbabbcac7517afbced3d4dc1cad8c
geekwise-korey-nichols <geekwise.korey.nichols@gmail.com>
2020-02-17 11:39:26 -0800

    added: bcrypt for password, login for users with token auth

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,8 +1,11 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-
+const keys = require('../config/keys');
 createUser = (req, res) => {
     const body = req.body
     console.log(body)
+
     if(!body) {
         return res.status(400).json({
             success: false,
@@ -16,22 +19,90 @@ createUser = (req, res) => {
         return res.status(400).json({success: false, error: err })
     }
 
-    user
-        .save()
-        .then( () => {
-            return res.status(201).json({
-                success: true,
-                id: user._id,
-                message: 'User Created!'
+    User.findOne({ email: req.body.email }).then( user => {
+        if(user) {
+            return res.status(400).json({email: 'email already exists'});
+        }
+
+        const newUser = new User(body)
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash( newUser.password, salt, (err, hash) => {
+                if(err) throw err;
+                newUser.password = hash;
+
+                newUser
+            .save()
+            .then( () => {
+                return res.status(201).json({
+                    success: true,
+                    id: newUser._id,
+                    message: 'User Created!'
+                })
+            })
+            .catch( error => {
+                return res.status(400).json({
+                    error,
+                    message: 'User not created!'
+                })
+            })
             })
         })
-        .catch( error => {
-            return res.status(400).json({
-                error,
-                message: 'User not created!'
-            })
-        })
+    })
 }
+
+deleteUser = async (req, res) => {
+    await User.findOneAndDelete({ _id: req.params.id }, (err, user) => {
+        if(err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+
+        if (!user) {
+            return res
+                .status(404)
+                .json({ success: false, error: `User not found` })
+        }
+
+        return res.status(200).json({ success: true, data: user })
+    }).catch(err => console.log(err))
+}
+
+login = (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email }).then(user => {
+        if(!user) {
+            return res.status(404).json({ success: false, emailNotFound: "email not found"})
+        }
+
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if(isMatch) {
+                const payload = {
+                    id: user.id,
+                    name: user.name
+                };
+            jwt.sign(
+                payload,
+                keys.secretOrKey,
+                {
+                    expiresIn: 31554926
+                },
+                (err, token) => {
+                    res.json({
+                        success: true,
+                        token: "Bearer " + token
+                    });
+                }
+            );
+            } else{
+                return res
+                    .status(400)
+                    .json({ passwordincorrect: "Password incorrect"});
+            }
+        });
+    });
+};
 
 getUsers = async (req, res) => {
     await User.find({}, (err, users) => {
@@ -49,5 +120,7 @@ getUsers = async (req, res) => {
 
 module.exports = {
     createUser,
+    deleteUser,
+    login,
     getUsers,
 }
--- backend/routes/user-router.js
+++ backend/routes/user-router.js
@@ -5,6 +5,8 @@ const UserController = require('../controllers/user-controller')
 const router = express.Router()
 
 router.post('/register', UserController.createUser)
+router.delete('/user/:id', UserController.deleteUser)
+router.post('/login', UserController.login)
 router.get('/users', UserController.getUsers)
 
 module.exports = router
